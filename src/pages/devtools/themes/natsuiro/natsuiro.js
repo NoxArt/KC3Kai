96dbef3706ef70fc98aed73faad0e644a3b611f0
sin_sin <sinsinpub@gmail.com>
2017-03-06 19:25:32 +0800

    Support live reloading meta manually via clicking panel button
    * Make natsuiro panel control buttons scrollable
    * Move natsuiro button images to assets/img/ui
    * New service to notify game tab reload meta
    
    TODO
    * Reload meta from remote repo instead of local json
    * Design reload button images for both quotes and quests

--- src/pages/devtools/themes/natsuiro/natsuiro.js
+++ src/pages/devtools/themes/natsuiro/natsuiro.js
@@ -627,6 +627,22 @@
 			}
 		});
 
+		// Scrollable control buttons
+		$(".module.controls .scroll_btn").on("click", function(){
+			var buttonCount = $(".module.controls .control_btn").length;
+			var buttonSize = $(".module.controls .control_btn").outerWidth(true);
+			var containerSize = $(".module.controls .scrollable").outerWidth(true);
+			var maxLeft = buttonSize * (buttonCount - Math.floor(containerSize / buttonSize));
+			var currentLeft = $(".module.controls .scrollable").scrollLeft();
+			var goLeft = $(this).hasClass("scroll_left");
+			var newLeft = goLeft ?
+				Math.max(currentLeft - buttonSize, 0) :
+				Math.min(currentLeft + buttonSize, maxLeft);
+			$(".module.controls .scrollable").scrollLeft(newLeft);
+			$(".module.controls .scroll_left").toggleClass("disabled", newLeft <= 0);
+			$(".module.controls .scroll_right").toggleClass("disabled", newLeft >= maxLeft);
+		});
+
 		// Resize window to 800x480
 		$(".module.controls .btn_resize").on("click", function(){
 			// Send fit-screen request to service to be forwarded to gameplay page
@@ -642,11 +658,34 @@
 				tabId: chrome.devtools.inspectedWindow.tabId
 			},function(isMuted){
 				if(isMuted){
-					$(".module.controls .btn_mute img").attr("src", "img/mute-x.png");
+					$(".module.controls .btn_mute img").attr("src", "../../../../assets/img/ui/mute-x.png");
 				}else{
-					$(".module.controls .btn_mute img").attr("src", "img/mute.png");
+					$(".module.controls .btn_mute img").attr("src", "../../../../assets/img/ui/mute.png");
 				}
 			})).execute();
+		});
+
+		// Reload subtitle quotes
+		$(".module.controls .btn_reload_quotes").on("click", function(){
+			// TODO request latest quotes.json for current lang from remote repo
+			// Tell game screen tab use latest meta
+			(new RMsg("service", "reloadMeta", {
+				tabId: chrome.devtools.inspectedWindow.tabId,
+				type: "Quotes"
+			})).execute();
+			// TODO add UI response to show reloading status
+		});
+
+		// Reload meta of quests
+		$(".module.controls .btn_reload_quests").on("click", function(){
+			// TODO request latest quests.json for both EN and current lang from remote repo
+			KC3Meta.reloadQuests();
+			// Tell game screen tab use latest meta
+			(new RMsg("service", "reloadMeta", {
+				tabId: chrome.devtools.inspectedWindow.tabId,
+				type: "Quests"
+			})).execute();
+			// TODO add UI response to show reloading status
 		});
 
 		// Trigger initial selected fleet num
@@ -694,7 +733,7 @@
 			tabId: chrome.devtools.inspectedWindow.tabId
 		}, function(isMuted){
 			if(isMuted){
-				$(".module.controls .btn_mute img").attr("src", "img/mute-x.png");
+				$(".module.controls .btn_mute img").attr("src", "../../../../assets/img/ui/mute-x.png");
 			}
 		})).execute();
 
@@ -734,11 +773,13 @@
 		// Wide interface, switch to vertical if not yet
 		if( $(window).width() >= 800 && currentLayout != "vertical" ){
 			$(".wrapper").removeClass("h").addClass("v");
-
 		// Narrow interface, switch to horizontal if not yet
 		}else if( $(window).width() < 800 && currentLayout != "horizontal" ){
 			$(".wrapper").removeClass("v").addClass("h");
 		}
+		$(".module.controls .scrollable").scrollLeft(0);
+		$(".module.controls .scroll_left").addClass("disabled");
+		$(".module.controls .scroll_right").removeClass("disabled");
 	}
 
 	function clearSortieData(){
